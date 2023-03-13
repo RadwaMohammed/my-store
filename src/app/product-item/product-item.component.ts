3e835982e545fc9b7a5aa64a2e0929ce184563ad
radwa <mail2radwa@gmail.com>
2023-03-13 09:49:13 +0200

    chore: Passing data between product-list to product-item

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from './../models/Product';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+  @Input() product: Product;
 
+  constructor() {
+    // Initialize the product
+    this.product = {
+      id: 1,
+      name: '',
+      price: 0,
+      url: '',
+      description: ''
+    }
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
