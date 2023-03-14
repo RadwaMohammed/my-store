fb986047d8f51fd5a4188575c9f661873f4343cf
radwa <mail2radwa@gmail.com>
2023-03-14 03:44:19 +0200

    chore: Generate header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
